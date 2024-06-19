905349aa181969a562a217e1d7597e39c2cdecbe
Ardelan <ardelanyamanel@outlook.de>
2024-06-19 13:26:12 +0200

    feat: Added utils and lucide icons

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function setTextRange(node: Node, offset: number) {
+	const selection = window.getSelection();
+
+	if (!selection) return;
+
+	const range = document.createRange();
+
+	range.setStart(node, offset);
+	range.collapse(true);
+
+	selection.removeAllRanges();
+	selection.addRange(range);
 }
