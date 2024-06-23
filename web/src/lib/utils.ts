b0c0a0a888c24ff27157d37ebe501fd4cd94c729
Ardelan <ardelanyamanel@outlook.de>
2024-06-23 14:33:35 +0200

    feat: Added setTextRange utility to set cursor at a specific position

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -6,6 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function setTextRange(node: Node, offset: number) {
+	if (!node) return;
+
 	const selection = window.getSelection();
 
 	if (!selection) return;
